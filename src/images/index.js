fd859b006fcffed2eb0aa6b7e749a0a498702ab1
fontainm <fontain@th-brandenburg.de>
2020-03-26 15:58:15 +0100

    First commit

---
+++ src/images/index.js
@@ -0,0 +1,11 @@
+
+export const memoryImages = [
+    require('./1.jpg'),
+    require('./2.jpg'),
+    require('./3.jpg'),
+    require('./4.jpg'),
+    require('./5.jpg'),
+    require('./6.jpg'),
+    require('./7.jpg'),
+    require('./8.jpg'),
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Tilt from 'react-tilt';
+
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Container from '@material-ui/core/Container';
+
+import { memoryImages } from './images';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function MemoryCard(props) {
+  return(
+    <Grid item xs={4} sm={4} md={3} xl={2}>
+      <Tilt className="Tilt" options={{ max : 30, scale: 1.1 }}>
+        <Card className="memoryCard">
+          <CardMedia 
+            style={props.faceDown ? { filter: 'grayscale(0%)' } : { filter: 'grayscale(100%)' } }
+            image={memoryImages[props.pairId]}
+            className="memoryImg"
+            onClick={() => props.onClick(props.id)}
+          />
+        </Card>
+      </Tilt>
+    </Grid>
+  );
+}
+
+class MemoryGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      memoryCards: shuffleCards(props.count)
+    };
+  }
+  
+  handleClick = id => {
+    let newMemoryCards = this.state.memoryCards.slice();
+    let matchedCard = newMemoryCards.find(function(card) { return card.id === id; } );
+    matchedCard.faceDown = !matchedCard.faceDown;
+    this.setState({
+      memoryCards: newMemoryCards
+    });
+  }
+
+  render() {
+    return(
+      <Container maxWidth={false}>
+        <Grid container spacing={3}>
+          { this.state.memoryCards.map((card, index) => {
+              return <MemoryCard key={card.id} id={card.id} pairId={card.pairId} faceDown={card.faceDown} onClick={(id) => this.handleClick(id)} />
+            }) }
+        </Grid>
+      </Container>
+    );
+  }
+}
+
+function shuffleCards(count) {
+  let memoryCards = [];
+  for (let i = 0; i < count*2; i++) {
+    memoryCards[i] = {
+      id: i,
+      pairId: Math.floor(i/2),
+      faceDown: true
+    };
+  }
+  memoryCards.sort(() => Math.random() - 0.5);
+  return memoryCards;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MemoryGame count={6} />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
